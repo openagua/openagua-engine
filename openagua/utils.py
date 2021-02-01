c5745dd575ce4d9402fbfd26a4270946ef236852
rheinheimer <herr.rhein@gmail.com>
2021-02-01 11:40:28 -0600

    misc updates, including removal of documentation from PyPI

--- openagua/utils.py
+++ openagua/utils.py
@@ -1,13 +1,13 @@
 from os import environ
+from openagua import constants
 
 
 def get_broker_url(model_key, protocol='amqp'):
-
     broker_url = '{protocol}://{username}:{password}@{hostname}:5672/{vhost}'.format(
         protocol=protocol,
         username=model_key,
         password='password',
-        hostname=environ.get('RABBITMQ_HOST', 'localhost'),
+        hostname=environ.get(constants.RABBITMQ_HOST, 'localhost'),
         vhost='model-{}'.format(model_key),
     )
 
